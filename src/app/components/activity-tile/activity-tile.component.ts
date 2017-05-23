49324665030c4ccacf96eabda0bbd5c0d82ebe41
a5trinh <anderson.trinh@gmail.com>
2017-05-22 22:56:08 -0400

    Testing out different component implimentations. *NgBootstrap added

---
+++ src/app/components/activity-tile/activity-tile.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'activity-tile',
+    templateUrl:'./activity-tile.component.html',
+    styleUrls:['./activity-tile.component.css']
+})
+
+export class ActivityTileComponent{
+
+}
---
+++ src/app/components/activity-tiles-container/activity-tiles-container.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+const DUMMYDATA = [
+  { item: 1 },
+  { item: 2 },
+  { item: 3 },
+  { item: 4 },
+  { item: 5 },
+  { item: 6 },
+  { item: 7 },
+  { item: 8 },
+  { item: 9 },
+  { item: 10 },
+  { item: 11 },
+  { item: 12 }
+];
+
+@Component({
+  selector: 'activity-tiles-container',
+  templateUrl: './activity-tiles-container.component.html',
+  styleUrls: ['./activity-tiles-container.component.css'],
+  providers: []
+})
+
+export class ActivityTilesContainerComponent {
+  first = 0;
+  last = 4;
+  dummyData = DUMMYDATA;
+}
